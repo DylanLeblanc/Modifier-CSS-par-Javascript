4461959db35c6b0c9d09fff68c8d22e4353b923f
DylanLeblanc <42961896+DylanLeblanc@users.noreply.github.com>
2018-09-04 16:45:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changerCouleur(){
+    document.getElementById("uneDiv").style.color = 'red';
+}
+
+function changerFond(){
+    document.getElementById("uneDiv").style.backgroundColor = 'red';
+}
+
+function augmenterHauteur(){
+    document.getElementById("uneDiv").style.height = "130px";
+}
+
+function diminuerHauteur(){
+    document.getElementById("uneDiv").style.height = "80px";
+}
